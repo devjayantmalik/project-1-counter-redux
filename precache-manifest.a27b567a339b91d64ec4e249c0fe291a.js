997d06ed4d66c6e328d4328c069bfba0106886c2
Jayant Malik <dev.jayantmalik@gmail.com>
2020-03-07 17:57:17 +0530

    Updates

---
+++ precache-manifest.a27b567a339b91d64ec4e249c0fe291a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1e4993c0478c363ddade292c3486ff",
+    "url": "/project-1-counter-redux/index.html"
+  },
+  {
+    "revision": "cbf23f74c45ca2aee0d7",
+    "url": "/project-1-counter-redux/static/js/2.efe70800.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project-1-counter-redux/static/js/2.efe70800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb7b167e75d7bdb3b87",
+    "url": "/project-1-counter-redux/static/js/main.c5bac67b.chunk.js"
+  },
+  {
+    "revision": "6f54001e43c8142eb51d",
+    "url": "/project-1-counter-redux/static/js/runtime-main.bb9f7040.js"
+  }
+]);
